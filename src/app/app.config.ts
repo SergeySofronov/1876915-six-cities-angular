af233013e1655c197d619155077b1ce4235ca14a
Сергей Софронов <nekromanser-box@yandex.ru>
2025-07-10 18:52:31 +0500

    ngrx install

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor, tokenInterceptor } from '@core/interceptors';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptors([
+    provideHttpClient(withInterceptors([
         tokenInterceptor,
         errorInterceptor,
-      ]),
-    )
-  ]
+    ])),
+    provideStore(),
+    provideEffects(),
+    provideRouterStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
